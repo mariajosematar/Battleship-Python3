945674eba9003685430145fa27dc1d1919bdef25
María José Mátar <80407624+mariajosematar@users.noreply.github.com>
2022-09-20 15:00:10 -0300

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,31 +1,3 @@
-"""
-LEGEND
-
-* X   for placing ship and hit battleship
-* ' ' for available splace
-* '-' for missed shot
-
-
-Game types 
-1. Single_player.py
-  - Computer randomly places 5 ships, player has 10 guesses
-2. Advanced_with_computer.py  
-  - player and computer place 5 single ships, both guess
-  - take turns guessing, whichever one sinks all five first wins
-3. 5_ship_types_with_computer.py
-  - 5 different ship types - 2, 3, 3, 4, 5
-4. Single_player_OO.py
-  - Rewrite Single Player to use Object oriented programming
-5. Future algorithm considerations
-  - tell user when ship is sunk
-  - Computer Placement 
-    - Place ships in center and not edges,
-    - cluster some ships, possibly in bottom right corner
-  - Computer Guess 
-    - Picker center, then checkerboard guess strategy
-    - If hit, computer plays adjacent to ship
-"""
-
 
 from curses import init_pair
 import random
